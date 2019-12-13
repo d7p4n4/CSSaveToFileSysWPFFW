8940de3c7ae3e5ff730f0824e40e8f2c594308de
d7p4n4 <bercikezs199999999@gmail.com>
2019-12-13 14:48:39 +0100

    Add project files.

---
+++ CSSaveToFileSysWPFFW/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using CSSaveToFileSysFW;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSSaveToFileSysWPFFW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public static string sqlConnectionString = ConfigurationManager.AppSettings["connString"];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveToFileSys(object subject, RoutedEventArgs e)
+        {
+            ComboBoxItem item = (ComboBoxItem)uiComboBox.SelectedItem;
+            string templateName = item.Content.ToString();
+            string path = uiTextBlock.Text.ToString();
+
+            uiListView.ItemsSource = new SaveToFileSysFW(
+                sqlConnectionString,
+                templateName,
+                path
+            ).WriteOutAc4yObjectNameList();
+
+        }
+    }
+}
